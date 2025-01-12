e43425654b3796d87359d4ac862f6e6db9ec5813
skillAndroid <sweetfoxnew@gmail.com>
2025-01-13 01:53:31 +0500

    just something tried to change

--- lib/features/details/presentation/pages/product_images_detailed.dart
+++ lib/features/details/presentation/pages/product_images_detailed.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:smooth_corner_updated/smooth_corner.dart';
 
-
-
 class ProductImagesDetailed extends StatefulWidget {
   final List<String> images;
   final int initialIndex;
@@ -26,13 +24,43 @@ class ProductImagesDetailed extends StatefulWidget {
 class _ProductImagesDetailedState extends State<ProductImagesDetailed> {
   late PageController _pageController;
   late int _currentIndex;
+  final List<TransformationController> _transformationControllers = [];
 
   @override
   void initState() {
     super.initState();
     _currentIndex = widget.initialIndex;
     _pageController = PageController(initialPage: widget.initialIndex);
+    for (var i = 0; i < widget.images.length; i++) {
+      _transformationControllers.add(TransformationController());
+    }
   }
+
+  @override
+  void dispose() {
+    for (var controller in _transformationControllers) {
+      controller.dispose();
+    }
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  // void _handleDoubleTap(TapDownDetails details, int index) {
+  //   final RenderBox renderBox = context.findRenderObject() as RenderBox;
+  //   final Offset localPosition =
+  //       renderBox.globalToLocal(details.globalPosition);
+
+  //   if (_transformationControllers[index].value != Matrix4.identity()) {
+  //     _transformationControllers[index].value = Matrix4.identity();
+  //   } else {
+  //     final Matrix4 matrix = Matrix4.identity()
+  //       ..translate(localPosition.dx * 0.75, localPosition.dy * 0.75)
+  //       ..scale(2.0)
+  //       ..translate(-localPosition.dx * 0.75, -localPosition.dy * 0.75);
+
+  //     _transformationControllers[index].value = matrix;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -53,24 +81,44 @@ class _ProductImagesDetailedState extends State<ProductImagesDetailed> {
           PageView.builder(
             controller: _pageController,
             onPageChanged: (index) {
-              setState(() => _currentIndex = index);
+              setState(() {
+                _currentIndex = index;
+                _transformationControllers[_currentIndex].value =
+                    Matrix4.identity();
+              });
             },
             itemCount: widget.images.length,
             itemBuilder: (context, index) {
-              return Center(
-                child: InteractiveViewer(
-                  minScale: 1.0,
-                  maxScale: 3.0,
-                  child: CachedNetworkImage(
-                    imageUrl: widget.images[index],
-                    fit: BoxFit.contain,
-                    placeholder: (context, url) => const Center(
-                      child: CircularProgressIndicator(
-                        color: Colors.white,
+              return LayoutBuilder(
+                builder: (context, constraints) {
+                  return Container(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight,
+                    color: Colors.black,
+                    child: GestureDetector(
+                      // onDoubleTapDown: (details) =>
+                      //     _handleDoubleTap(details, index),
+                      onDoubleTap: () {},
+                      child: InteractiveViewer(
+                        transformationController:
+                            _transformationControllers[index],
+                        minScale: 1.0,
+                        maxScale: 4.0,
+                        child: Center(
+                          child: CachedNetworkImage(
+                            imageUrl: widget.images[index],
+                            fit: BoxFit.contain,
+                            placeholder: (context, url) => const Center(
+                              child: CircularProgressIndicator(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
+                  );
+                },
               );
             },
           ),
