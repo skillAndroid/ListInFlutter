89454b0c8274efc52b98f2d6d732585b20f58cc6
skillAndroid <sweetfoxnew@gmail.com>
2025-03-10 14:27:37 +0500

    remoute explore translation is startted!

--- lib/features/explore/presentation/widgets/category_card.dart
+++ lib/features/explore/presentation/widgets/category_card.dart
@@ -5,12 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:list_in/config/theme/app_colors.dart';
+import 'package:list_in/config/theme/app_language.dart';
+import 'package:list_in/core/language/language_bloc.dart';
 import 'package:list_in/core/router/routes.dart';
 import 'package:list_in/core/utils/const.dart';
 import 'package:list_in/features/explore/presentation/bloc/cubit.dart';
 import 'package:list_in/features/explore/presentation/bloc/state.dart';
 import 'package:list_in/features/post/data/models/category_model.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:list_in/features/post/presentation/pages/atributes_releted/child_category_page.dart';
 
 class CategoryCard extends StatefulWidget {
   final HomeTreeState state;
@@ -64,113 +66,127 @@ class _CategoryCardState extends State<CategoryCard>
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 5),
-      child: GestureDetector(
-        onTap: () {
-          context.read<HomeTreeCubit>().selectCatalog(widget.category);
-          context.goNamed(RoutesByName.subcategories, extra: {
-            'category': widget.category,
-            'priceFrom': widget.state.priceFrom,
-            'priceTo': widget.state.priceTo,
-            'filterState': {
-              'bargain': widget.state.bargain,
-              'isFree': widget.state.isFree,
-              'condition': widget.state.condition,
-              'sellerType': widget.state.sellerType,
+    return BlocSelector<LanguageBloc, LanguageState, String>(
+      selector: (state) =>
+          state is LanguageLoaded ? state.languageCode : AppLanguages.english,
+      builder: (context, languageCode) {
+        return Padding(
+          padding: const EdgeInsets.only(right: 5),
+          child: GestureDetector(
+            onTap: () {
+              context.read<HomeTreeCubit>().selectCatalog(widget.category);
+              context.goNamed(RoutesByName.subcategories, extra: {
+                'category': widget.category,
+                'priceFrom': widget.state.priceFrom,
+                'priceTo': widget.state.priceTo,
+                'filterState': {
+                  'bargain': widget.state.bargain,
+                  'isFree': widget.state.isFree,
+                  'condition': widget.state.condition,
+                  'sellerType': widget.state.sellerType,
+                },
+              });
             },
-          });
-        },
-        onTapDown: (_) {
-          _scaleController.reverse();
-        },
-        onTapUp: (_) {
-          _scaleController.forward();
-        },
-        onTapCancel: () {
-          _scaleController.forward();
-        },
-        child: ScaleTransition(
-          scale: _scaleController,
-          child: Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
-                decoration: BoxDecoration(
-                  color: AppColors.containerColor,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(width: 6),
-                    ConstrainedBox(
-                      constraints: BoxConstraints(
-                        maxWidth: widget.maxWidth,
-                      ),
+            onTapDown: (_) {
+              _scaleController.reverse();
+            },
+            onTapUp: (_) {
+              _scaleController.forward();
+            },
+            onTapCancel: () {
+              _scaleController.forward();
+            },
+            child: ScaleTransition(
+              scale: _scaleController,
+              child: Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 0),
+                    decoration: BoxDecoration(
+                      color: AppColors.containerColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(width: 6),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth: widget.maxWidth,
+                          ),
+                          child: Text(
+                            getLocalizedText(
+                                widget.category.name,
+                                widget.category.nameUz,
+                                widget.category.nameRu,
+                                languageCode),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            softWrap: true,
+                            style: const TextStyle(
+                              fontSize: 11,
+                              color: AppColors.transparent,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: Constants.Arial,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                            width: widget.maxWidth,
+                            height: widget.height,
+                            child: SizedBox()),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 8,
+                    left: 8,
+                    child: SizedBox(
+                      width: widget.maxWidth,
                       child: Text(
-                        widget.category.name,
+                       getLocalizedText(
+                                widget.category.name,
+                                widget.category.nameUz,
+                                widget.category.nameRu,
+                                languageCode),
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                         softWrap: true,
                         style: const TextStyle(
-                          fontSize: 11,
-                          color: AppColors.transparent,
+                          fontSize: 12,
+                          color: AppColors.black,
                           fontWeight: FontWeight.w500,
-                         fontFamily: Constants.Arial,
+                          fontFamily: Constants.Arial,
                         ),
                       ),
                     ),
-                    SizedBox(
-                        width: widget.maxWidth,
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Transform.translate(
+                      offset: widget.imageOffset,
+                      child: SizedBox(
+                        width: widget.width,
                         height: widget.height,
-                        child: SizedBox()),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 8,
-                left: 8,
-                child: SizedBox(
-                  width: widget.maxWidth,
-                  child: Text(
-                    widget.category.name,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    softWrap: true,
-                    style: const TextStyle(
-                      fontSize: 12,
-                      color: AppColors.black,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: Constants.Arial,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Transform.translate(
-                  offset: widget.imageOffset,
-                  child: SizedBox(
-                    width: widget.width,
-                    height: widget.height,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(
-                        widget.radius,
-                      ),
-                      child: CachedNetworkImage(
-                        imageUrl: widget.category.logoUrl,
-                        fit: widget.imageFit,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                            widget.radius,
+                          ),
+                          child: CachedNetworkImage(
+                            imageUrl: widget.category.logoUrl,
+                            fit: widget.imageFit,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
